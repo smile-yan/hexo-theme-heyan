98fdfa24376e518a5c03712871eddb8beb4a2b95
yanshili <root@smileyan.cn>
2022-04-25 11:10:53 +0800

    feat: add mathjax util scripts

---
+++ scripts/mathjax_util.js
@@ -0,0 +1,46 @@
+console.log('welcome')
+
+// 根据 $$ 将原内容分块
+// 如果是奇数块则前后添加 $$ 符号
+function dollarsHandle(data) {
+     var math_blocks = data.split('$$')
+     var newContent = ""
+     if (math_blocks.length > 2) {
+         math_blocks.forEach((one, i)=>{
+            if (i % 2 == 1 ) {
+                newContent += '<p>$$'
+                newContent += one
+                newContent += '$$</p>'
+             } else {
+                 newContent += one
+             }
+         })
+        return newContent
+     }
+     return data
+}
+
+
+hexo.extend.filter.register('before_post_render', data => {
+    
+    // 首先根据 ``` 划分为代码块与非代码块
+    var blocks = data.content.split('```')
+    blocks.forEach((block, i) => {
+        // 如果是偶数块
+        if (i % 2 == 0) {
+            blocks[i] = dollarsHandle(block)
+        }
+    })
+
+    if (blocks.length > 2) {
+        var newDataContent = ""
+        blocks.forEach((block, i)=>{
+            newDataContent += block
+            newDataContent += "```"
+        })
+        data.content = newDataContent
+    } else {
+        data.content = dollarsHandle(data.content)
+    }
+    return data;
+  }, 5);
