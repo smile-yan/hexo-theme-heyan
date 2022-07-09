dbb51518dfebdfb98cf5ac2600bd2d3588081e6a
smileyan <root@smileyan.cn>
2022-07-10 00:59:32 +0800

    feat: update search style

--- source/js/search.js
+++ source/js/search.js
@@ -20,8 +20,7 @@ var searchFunc = function(path, search_id, content_id) {
             if ($("#local-search-input").length > 0) {
                 $input.addEventListener('input', function () {
                     var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                    // var str = '<h3 class="tag-hover">搜索内容: '+ keywords +'<i class="fa fa-close ps-2 small" onclick="clearSearch()"></i></h3><ul class=\"list-group pb-3\">';
-                    var str = '<ul class=\"list-group pb-3\">'
+                    var str = '<ul class=\"list-group pb-3\">';
                     $resultContent.innerHTML = "";
                     if (this.value.trim().length <= 0) {
                         return;
@@ -62,7 +61,6 @@ var searchFunc = function(path, search_id, content_id) {
                         }
                         // show search results
                         if (isMatch) {
-                            str += "<li class='list-group-item tag-hover'><a href='" + data_url + "' class='search-result-title link-dark'>" + data_title +"</a>";
                             var content = data.content.trim().replace(/<[^>]+>/g, "");
                             if (first_occur >= 0) {
                                 // cut out 100 characters
@@ -89,6 +87,12 @@ var searchFunc = function(path, search_id, content_id) {
                                     match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
                                 });
 
+                                keywords.forEach(function (keyword) {
+                                    var regS = new RegExp(keyword, "gi");
+                                    data_title = data_title.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
+                                });
+
+                                str += "<li class='list-group-item tag-hover'><a href='" + data_url + "' class='search-result-title link-dark'>" + data_title +"</a>";
                                 str += "<p class=\"search-result\">" + match_content + "...</p>"
                             }
                             str += "</li>";
@@ -103,7 +107,7 @@ var searchFunc = function(path, search_id, content_id) {
 }
 
 function clearSearch() {
-    $('.local-search-input').val("")
+    $('#local-search-input').val("")
     $('#local-search-result').empty()
 
 }   
